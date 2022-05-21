de67aec6412f783df95924902db5aba2870547f1
bharathmanohar96 <102683198+bharathmanohar96@users.noreply.github.com>
2022-05-21 16:47:58 +0530

    Create tomcatcdci.groovy

---
+++ var/tomcatcdci.groovy
@@ -0,0 +1,8 @@
+def call(tomcatid,warname,credid){
+  sshagent(['${credid}']) {
+                // some block
+      sh "scp -o StrictHostKeyChecking=no target/*.war ec2-user@${tomcatid}:/opt/tomcat8/webapps/${warname}.war"
+      sh "ssh ec2-user@${tomcatid} /opt/tomcat8/bin/shutdown.sh"
+      sh "ssh ec2-user@${tomcatid} /opt/tomcat8/bin/startup.sh"
+    }
+}
